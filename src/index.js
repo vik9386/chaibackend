c1053eb159fc71bec55432a5dd4b06beed84d2e3
vik9386 <kmr.vikash1994@gmail.com>
2024-07-10 23:39:25 +0530

    middleware set part 1

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constatnts";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:"./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
 
+    app.on("error",(error)=>{
+        console.log("Error ",error)
+        throw error;
+    })
 
-
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err)
+})
 
 
 
